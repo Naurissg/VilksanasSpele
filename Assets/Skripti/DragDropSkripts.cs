2e19528506268c4273f546b89e8cbd026b01138e
Nauris <naurissgg@gmail.com>
2023-05-24 10:38:53 +0300

    Pievienota iespeja drag

---
+++ Assets/Skripti/DragDropSkripts.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//jaieimporte lai varetu lietot visus I interface
+using UnityEngine.EventSystems;
+
+
+public class DragDropSkripts : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler{
+	//Uzglabá norádi uz Objekta Skriptu
+	public Objekti objektuSkripts;
+	private CanvasGroup kanvasGrupa;
+	private RectTransform velkObjRectTransf;
+
+	public void OnBeginDrag(PointerEventData eventData){
+		objektuSkripts.pedejaisVilktais = null;
+		kanvasGrupa.alpha = 0.6f;
+		kanvasGrupa.blocksRaycasts = false;
+	}
+
+	public void OnDrag(PointerEventData eventData){
+		velkObjRectTransf.anchoredPosition += eventData.delta / objektuSkripts.kanva.scaleFactor;
+	}
+	public void OnEndDrag (PointerEventData eventData){
+		objektuSkripts.pedejaisVilktais = eventData.pointerDrag;
+		kanvasGrupa.alpha = 1f;
+
+		if (objektuSkripts.vaiIstajaVieta == false) {
+			kanvasGrupa.blocksRaycasts = true;
+
+		} else {
+			objektuSkripts.pedejaisVilktais = null;
+		}
+		objektuSkripts.vaiIstajaVieta = false;
+	}
+
+	void Start () {
+		//peiklust velkama objekta CnavasGroup komponentei
+		kanvasGrupa = GetComponent<CanvasGroup>();
+		//Pieklust velkama objekta RectTransform komponentei
+		velkObjRectTransf = GetComponent<RectTransform>();
+	}
+}
---
+++ Assets/Skripti/Objekti.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objekti : MonoBehaviour {
+	//Uzglabas visus velkamos objektus
+	public GameObject atkritumuMasina;
+	public GameObject atraPalidziba;
+	public GameObject autobuss;
+
+	//uzglabas katra transortlidzekla sakotnejo atraśanas vietu
+	[HideInInspector]
+	public Vector2 atkrMKoord;
+	[HideInInspector]
+	public Vector2 atrPKoord;
+	[HideInInspector]
+	public Vector2 bussKoord;
+
+	public Canvas kanva;
+
+	//Uzglabas audio avotu, kura atskańot audio efektus
+	public  AudioSource skanasAvots;
+	//uzglabas audio failus
+	public AudioClip[] skanaKoAtskanot;
+
+	[HideInInspector]
+	public bool vaiIstajaVieta = false;
+	[HideInInspector]
+	public GameObject pedejaisVilktais = null;
+
+	void start (){
+		//uzsakot speli piefikse kur sakotneji atrodas katra masina
+		atkrMKoord = 
+		atkritumuMasina.GetComponent<RectTransform>().localPosition;
+		atrPKoord = 
+		atkritumuMasina.GetComponent<RectTransform>().localPosition;
+		bussKoord = 
+		atkritumuMasina.GetComponent<RectTransform>().localPosition;
+	}
+}
